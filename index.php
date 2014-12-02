6674558e7684689a055a11527dd7518fc2978e9b
gekko-rails <a----k@ya.ru>
2014-12-03 02:39:38 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <title>For sale by gekko-rails</title>
+
+    <link rel="stylesheet" href="stylesheets/styles.css">
+    <link rel="stylesheet" href="stylesheets/pygment_trac.css">
+    <script src="javascripts/scale.fix.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="wrapper">
+      <header>
+        <h1>For sale</h1>
+        <p>Make it yours</p>
+        <p class="view"><a href="https://github.com/gekko-rails/mydomains">View the Project on GitHub <small>gekko-rails/mydomains</small></a></p>
+        <ul>
+          <li><a href="https://github.com/gekko-rails/mydomains/zipball/master">Download <strong>ZIP File</strong></a></li>
+          <li><a href="https://github.com/gekko-rails/mydomains/tarball/master">Download <strong>TAR Ball</strong></a></li>
+          <li><a href="https://github.com/gekko-rails/mydomains">View On <strong>GitHub</strong></a></li>
+        </ul>
+      </header>
+      <section>
+        <h1>
+<a id="this-domain-is-for-sale" class="anchor" href="#this-domain-is-for-sale" aria-hidden="true"><span class="octicon octicon-link"></span></a>This domain is for sale</h1>
+
+<p>Write for more information: <a href="mailto:projects@factory9.ru">projects@factory9.ru</a></p>
+
+<p>You can also download the list of other available domains.</p>
+      </section>
+    </div>
+    <footer>
+      <p>Project maintained by <a href="https://github.com/gekko-rails">gekko-rails</a></p>
+      <p>Hosted on GitHub Pages &mdash; Theme by <a href="https://github.com/orderedlist">orderedlist</a></p>
+    </footer>
+    <!--[if !IE]><script>fixScale(document);</script><![endif]-->
+              <script type="text/javascript">
+            var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+            document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+          </script>
+          <script type="text/javascript">
+            try {
+              var pageTracker = _gat._getTracker("UA-57276124-1");
+            pageTracker._trackPageview();
+            } catch(err) {}
+          </script>
+
+  </body>
+</html>
